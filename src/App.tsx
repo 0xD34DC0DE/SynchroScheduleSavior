d9d804f8ae2fce5de4d3fd56be1ff7b83ef101e9
Émerick Poulin <emerick.poulin@omnirobotic.com>
2023-12-22 19:45:18 -0500

    tweak initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import {Box} from "@mui/material";
 
 function App() {
   return (
-    <></>
+    <Box sx={{
+      width: "100%",
+      height: "100%",
+    }}>
+
+    </Box>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {CssBaseline} from "@mui/material";
+import "./style.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <CssBaseline/>
+        <App/>
+    </React.StrictMode>,
 );
