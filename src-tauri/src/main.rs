6ad9269a6b7a066e7636763d741f4f712cf07fe5
Émerick Poulin <emerick.poulin@omnirobotic.com>
2023-12-26 17:39:18 -0500

    retrieve return value of injected javascript functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
+use rand::Rng;
+use serde_json::Value;
 use tauri::{AppHandle, Manager};
+use tokio::sync::mpsc;
+use tokio::time::{self, Duration};
 use url::Url;
 
 #[tauri::command]
@@ -31,13 +35,48 @@ async fn close_puppet(handle: AppHandle) -> Result<(), String> {
 }
 
 #[tauri::command]
-async fn synchro_inject(javascript: String, handle: AppHandle) -> Result<(), String> {
+async fn synchro_inject(js: String, timeout_ms: u64, handle: AppHandle) -> Result<Value, String> {
     let window = handle.get_window("synchro");
     if window.is_none() {
         return Err("Synchro puppet window not open".to_string());
     }
     let window = window.unwrap();
-    window.eval(javascript.as_ref()).map_err(|e| e.to_string())
+
+    let (tx, mut rx) = mpsc::channel::<String>(1);
+
+    let event_id = format!("synchro-extraction-{}", rand::thread_rng().gen::<u16>());
+
+    let event_handler = window.listen(event_id.clone(), move |extraction_event| {
+        if let Some(payload) = extraction_event.payload() {
+            match tx.try_send(payload.to_string()) {
+                Ok(_) => {},
+                Err(e) => {
+                    eprintln!("Error sending extracted data: {}", e);
+                }
+            }
+        }
+    });
+
+    let extraction_wrapper = format!("window.__TAURI__.event.emit('{}', ({})());", event_id, js);
+
+    window.eval(extraction_wrapper.as_ref()).map_err(|e| e.to_string())?;
+
+    let result = match time::timeout(Duration::from_millis(timeout_ms), rx.recv()).await {
+        Ok(r) => match r {
+            Some(r) => Ok(r),
+            None => Err("Extraction did not send any data".to_string()),
+        }
+        Err(e) => Err(e.to_string()),
+    };
+
+    window.unlisten(event_handler);
+
+    let result = serde_json::to_value(result).map_err(|e| e.to_string())?;
+    match result.get("Ok") {
+        Some(Value::String(s)) => Ok(serde_json::from_str(s.as_str()).map_err(|e| e.to_string())?),
+        Some(v) => Err(format!("Extraction error, result is not a string: {}", v)),
+        None => Err("Extraction did not send any data".to_string()),
+    }
 }
 
 fn main() {
