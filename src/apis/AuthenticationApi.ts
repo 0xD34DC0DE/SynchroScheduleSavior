881b360c784d12299937729f4740e35c9bed6582
Émerick Poulin <emerick.poulin@omnirobotic.com>
2023-12-30 17:27:44 -0500

    remove unused files

--- src/apis/AuthenticationApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const useAuthenticationApi = () => {
-    login: async (username: string, password: string, cancel: boolean = false) => {
-        const response = await axios.post('/api/login', {username, password, cancel});
-        return response.data;
-    }
-}
