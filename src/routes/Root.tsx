413b4f29f15bb59ebff296db380001580c242417
Émerick Poulin <emerick.poulin@omnirobotic.com>
2023-12-31 22:28:03 -0500

    navigate to landing page on data loaded

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,22 @@
 import {CssBaseline} from "@mui/material";
 import FlexBox from "../components/FlexBox.tsx";
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
+import useDataStore from "../stores/dataStore.ts";
+import {useEffect} from "react";
 
 export interface RootProps {
 }
 
 const Root = ({}: RootProps) => {
+    const dataHasHydrated = useDataStore(state => state._hasHydrated);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (dataHasHydrated) {
+            navigate("/landing");
+        }
+    }, [dataHasHydrated]);
+
     return (
         <CssBaseline>
             <FlexBox sx={{width: "100%", height: "100%", backgroundColor: "rgb(255,247,240)"}}
