9c89d8916b6e00ad5723024cf312f97537fe1c42
Émerick Poulin <emerick.poulin@omnirobotic.com>
2024-01-01 02:50:27 -0500

    add routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import Root from "./routes/Root.tsx";
 import LandingPage from "./routes/LandingPage.tsx";
+import DataCollectionPage from "./routes/DataCollectionPage.tsx";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,14 @@ const router = createBrowserRouter([
                 path: "/landing",
                 element: <LandingPage/>,
             },
+            {
+                path: "/dashboard",
+                element: <div>TODO - Dashboard</div>,
+            },
+            {
+                path: "/data_collection",
+                element: <DataCollectionPage/>,
+            }
         ],
     },
 ]);
