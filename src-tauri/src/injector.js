0f5f6f0af3efa92775026dc1549f13866529696c
Émerick Poulin <emerick.poulin@omnirobotic.com>
2024-01-02 20:14:00 -0500

    wait for document ready before trying to inject

--- src-tauri/src/injector.js
+++ src-tauri/src/injector.js
@@ -1,29 +1,35 @@
 window.__INJECTOR__ = (event_name, fn, expected_type, args) => {
-    const ok = (val) => window.__TAURI__.event.emit(event_name, {ok: val});
-    const ok_special = (val) => window.__TAURI__.event.emit(event_name, {ok: val, special: true});
-    const err = (val) => window.__TAURI__.event.emit(event_name, {err: val});
+    let state_check = setInterval(() => {
+        if (document.readyState === 'complete') {
+            clearInterval(state_check);
 
-    try {
-        const result = fn(...args);
+            const ok = (val) => window.__TAURI__.event.emit(event_name, {ok: val});
+            const ok_special = (val) => window.__TAURI__.event.emit(event_name, {ok: val, special: true});
+            const err = (val) => window.__TAURI__.event.emit(event_name, {err: val});
 
-        if (typeof result !== expected_type) {
-            err(`Returned value does not match expected type, expected ${expected_type}, got ${typeof result}`);
-        } else if (result === undefined) {
-            ok_special("undefined");
-        } else if (result === null) {
-            ok_special("null");
-        } else if (Number.isNaN(result)) {
-            ok_special("NaN");
-        } else if (result === Infinity) {
-            ok_special("Infinity");
-        } else if (result === -Infinity) {
-            ok_special("-Infinity");
-        } else {
-            ok(result);
+            try {
+                const result = fn(...args);
+
+                if (typeof result !== expected_type) {
+                    err(`Returned value does not match expected type, expected ${expected_type}, got ${typeof result}`);
+                } else if (result === undefined) {
+                    ok_special("undefined");
+                } else if (result === null) {
+                    ok_special("null");
+                } else if (Number.isNaN(result)) {
+                    ok_special("NaN");
+                } else if (result === Infinity) {
+                    ok_special("Infinity");
+                } else if (result === -Infinity) {
+                    ok_special("-Infinity");
+                } else {
+                    ok(result);
+                }
+
+                err("Function returned an value of an unsupported type");
+            } catch (e) {
+                err(e.toString());
+            }
         }
-
-        err("Function returned an value of an unsupported type");
-    } catch (e) {
-        err(e.toString());
-    }
+    }, 100);
 }
