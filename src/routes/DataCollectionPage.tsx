bf7b89100400b9ab7b95cb59c8e9f72e3e377200
Émerick Poulin <emerick.poulin@omnirobotic.com>
2024-01-03 12:42:51 -0500

    add Semester selection step

--- src/routes/DataCollectionPage.tsx
+++ src/routes/DataCollectionPage.tsx
@@ -1,6 +1,7 @@
 import LoginStep from "../components/LoginStep.tsx";
 import OnboardingStepper from "../components/OnboardingStepper.tsx";
 import OnboardingStep from "../components/OnboardingStep.tsx";
+import SemesterSelectionStep from "../components/SemesterSelectionStep.tsx";
 
 export interface DataCollectionPageProps {
 
@@ -17,6 +18,9 @@ const DataCollectionPage = ({}: DataCollectionPageProps) => {
         >
             <OnboardingStep label={"Login"} step_key={"login"}>
                 <LoginStep/>
+            </OnboardingStep>
+            <OnboardingStep label={"Semesters"} step_key={"semesters"}>
+                <SemesterSelectionStep/>
             </OnboardingStep>
         </OnboardingStepper>
     );
