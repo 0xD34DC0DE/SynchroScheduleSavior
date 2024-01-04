da5547afa94dbc1a91d1625ddc5e4b926c845302
Émerick Poulin <emerick.poulin@omnirobotic.com>
2024-01-04 14:46:13 -0500

    use state getter

--- src-tauri/src/web_view_injector/window_builder.rs
+++ src-tauri/src/web_view_injector/window_builder.rs
@@ -1,7 +1,7 @@
 use tauri::{Manager, Runtime, WindowUrl};
 use tauri::window::WindowBuilder;
 
-use crate::web_view_injector::state::WebviewInjectorStateType;
+use crate::web_view_injector::state::{StateManagerExtInternal};
 
 pub struct InjectableWindowBuilder {}
 
@@ -9,6 +9,7 @@ impl<'a> InjectableWindowBuilder {
     pub fn new<M: Manager<R>, L: Into<String>, R: Runtime>(manager: &'a M, label: L, url: WindowUrl) -> WindowBuilder<'a, R> {
         manager.state::<WebviewInjectorStateType<R>>().lock().unwrap().register_window(label.into());
         WindowBuilder::new(manager, label, url)
+        manager.get_state().register_window(label.into());
             .initialization_script(include_str!("injection_handler.js"))
     }
 }
