37d96e2b58f8f30f0fd7a0146803680b974bfaa6
Émerick Poulin <emerick.poulin@omnirobotic.com>
2024-01-10 12:17:57 -0500

    InterWebviewPromise constructor

--- src-tauri/src/web_view_injector/inter_webview_promise.rs
+++ src-tauri/src/web_view_injector/inter_webview_promise.rs
@@ -1,34 +1,69 @@
-use std::time::Duration;
-use tokio::sync::oneshot::Receiver;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use rand::Rng;
+use serde::{Deserialize, Serialize};
+use tauri::{EventHandler, Runtime, Window};
+use tokio::sync::oneshot::{self, Receiver};
 
 pub struct InterWebviewPromise {
-    webview_receiver: Receiver<Result<String>>,
-    result_receiver: Receiver<Result<String>>,
-    timeout: Duration,
-    // Need another Receiver to hold the result until the front-end requests it
-    // Need another since the first one might receive nothing (timeout) and we still need
-    // something to hold onto that error (or success) until the front-end requests it
+    target_receiver: Receiver<Result<String>>,
+    target_listener: EventHandler,
+    handles: InterWebviewPromiseHandles,
 }
 
 impl InterWebviewPromise {
-    pub fn new(event_name_prefix: &'static str) -> Self {
-        todo!()
+    pub fn new<R: Runtime>(target: &Window<R>, event_name_prefix: &'static str) -> Self {
+        let handles = InterWebviewPromiseHandles::new(event_name_prefix);
+
+        let (target_sender, target_receiver) = oneshot::channel();
+        let target_listener = target.once(handles.result.as_ref(), move |event| {
+            if target_sender.is_closed() { return; }
+            target_sender.send(
+                event
+                    .payload()
+                    .map(|payload| payload.to_string())
+                    .ok_or(anyhow!("No payload in event"))
+            ).expect("Failed to send result to initiator");
+        });
+
+        Self {
+            target_receiver,
+            target_listener,
+            handles,
+        }
+    }
+
+    pub fn handles(&self) -> &InterWebviewPromiseHandles {
+        &self.handles
     }
 }
 
-struct UniqueEventsName {
-    pub result: String,
-    pub cancel: String,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PromiseEventHandle(String);
+
+impl PromiseEventHandle {
+    pub fn new(event_name: String) -> Self {
+        Self(event_name)
+    }
 }
 
-impl UniqueEventsName {
+impl AsRef<str> for PromiseEventHandle {
+    fn as_ref(&self) -> &str {
+        self.0.as_ref()
+    }
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct InterWebviewPromiseHandles {
+    pub result: PromiseEventHandle,
+    pub cancel: PromiseEventHandle,
+}
+
+impl InterWebviewPromiseHandles {
     pub fn new(prefix: &'static str) -> Self {
         let rng = rand::thread_rng().gen::<u16>();
         Self {
-            result: format!("{}-{:x}", prefix, rng),
-            cancel: format!("{}-cancel-{:x}", prefix, rng),
+            result: PromiseEventHandle::new(format!("{}-{:x}", prefix, rng)),
+            cancel: PromiseEventHandle::new(format!("{}-cancel-{:x}", prefix, rng)),
         }
     }
 }
