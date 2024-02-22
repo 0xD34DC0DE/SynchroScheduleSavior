1868b4349901de2264c356e1a9d5e89f77e06389
Émerick Poulin <emerickpoulin31415@gmail.com>
2024-02-22 16:22:37 -0500

    add typescript commands API

---
+++ src/scraper/src/commands.ts
@@ -0,0 +1,36 @@
+import {invoke} from '@tauri-apps/api/tauri';
+
+const open_webview = async (windowLabel: string, url: string): Promise<void> => {
+    await invoke<void>('open_webview', {windowLabel, url});
+}
+
+const close_webview = async (windowLabel: string): Promise<void> => {
+    await invoke<void>('close_webview', {windowLabel});
+}
+
+
+type Serializable = string | number | boolean | null | Serializable[] | { [key: string]: Serializable };
+
+const webview_inject = async <F extends (...args: Parameters<F>) => ReturnType<F>, T extends ReturnType<F>>(
+    targetWindowLabel: string,
+    injectionId: number,
+    jsFunction: F,
+    args: Parameters<F>,
+    allowParallel: boolean = false,
+): Promise<ReturnType<F>> => {
+    return await invoke<T>('webview_inject', {
+        targetWindowLabel,
+        injectionId,
+        jsFunction: jsFunction.toString(),
+        allowParallel,
+        args
+    });
+}
+
+export {
+    open_webview,
+    close_webview,
+    webview_inject
+};
+
+export type {Serializable};
