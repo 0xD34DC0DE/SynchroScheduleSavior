ddcd077cfdb210c66ccdc341a5b1f7461d5226f1
Émerick Poulin <emerick.poulin@omnirobotic.com>
2024-03-10 21:49:22 -0400

    temporary testing page

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,17 @@ import {createBrowserRouter} from "react-router-dom";
 import Root from "./routes/Root.tsx";
 import LandingPage from "./routes/LandingPage.tsx";
 import DataCollectionPage from "./routes/DataCollectionPage.tsx";
+import Testing from "./routes/Testing.tsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root/>,
         children: [
+            {
+                path: "/test",
+                element: <Testing/>,
+            },
             {
                 path: "/landing",
                 element: <LandingPage/>,
