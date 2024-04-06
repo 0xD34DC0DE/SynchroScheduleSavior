3643b6be7f1733a5c1a8b1ace68c395297cc0ea9
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-04-06 16:38:12 -0400

    try injection with context

--- src/routes/Testing.tsx
+++ src/routes/Testing.tsx
@@ -4,7 +4,9 @@ import {useState} from "react";
 import {Link} from "react-router-dom";
 import {PipelineState} from "../scraper/src/pipeline/task_pipeline.ts";
 import login_modal_html from "../assets/login_modal.html?raw";
-import { Context } from "../scraper/src/context.ts";
+import {Context} from "../scraper/src/context.ts";
+import {once} from "@tauri-apps/api/event";
+import {getCurrent} from "@tauri-apps/api/window";
 
 export interface TestingProps {
 
@@ -18,23 +20,26 @@ const Testing = ({}: TestingProps) => {
         scraper
             .begin()
             //.wait_for_url("*/NUI_FRAMEWORK.PT_LANDINGPAGE.GBL?")
-            .task(
-                (login_modal_html: string) => {
+            .taskWithContext(Context,
+                (ctx, modal_html: string) => {
                     const placeholder = document.createElement("div");
-                    placeholder.innerHTML = login_modal_html;
+                    placeholder.innerHTML = modal_html;
                     document.body.appendChild(placeholder);
                     const focus_button = document.getElementById("back-to-main-window");
                     if (!focus_button) throw new Error("Button not found");
-                    focus_button.onclick = () => {
-                        //TODO: Call event emitter
+                    focus_button.onclick = async () => {
+                        await ctx.initiator.emit("back-to-main-window");
                     };
                 },
                 [login_modal_html],
                 (r) => {
                     if ("error" in r) {
-                        console.error(r.error);
+                        console.error("Injector error:", r.error);
                     } else {
-                        console.log("Task completed");
+                        console.log("Task completed, starting listener");
+                        once("back-to-main-window", async () => {
+                           await getCurrent().setFocus();
+                        }).then(() => {});
                     }
                 }
             )
