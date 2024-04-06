687f672a65fca90c884464ae27536b6d921c1975
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-04-06 01:30:17 -0400

    use weaker types on webview_inject command

--- src/scraper/src/commands.ts
+++ src/scraper/src/commands.ts
@@ -9,18 +9,18 @@ const close_webview = async (windowLabel: string): Promise<void> => {
     await invoke<void>('close_webview', {windowLabel});
 }
 
-const webview_inject = async <F extends (...args: Parameters<F>) => ReturnType<F>>(
+const webview_inject = async(
     targetWindowLabel: string,
     injectionId: number,
-    jsFunction: F,
-    args: Parameters<F>,
+    jsFunction: string,
+    args: any[] = [],
     allowParallel: boolean = false,
 ): Promise<void> => {
     console.log("(cmd::webview_inject) ", targetWindowLabel, injectionId, jsFunction, args, allowParallel);
     return await invoke('webview_inject', {
         targetWindowLabel,
         injectionId,
-        jsFunction: jsFunction.toString(),
+        jsFunction,
         allowParallel,
         args
     });
