3b8652ccc5b2f1a453df366e37d625d8ae1a3e30
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-04-09 23:04:30 -0400

    remove old components and pages

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,17 +1,13 @@
 import {CssBaseline} from "@mui/material";
-import FlexBox from "../components/FlexBox.tsx";
 import {Outlet} from "react-router-dom";
 
-export interface RootProps {
+interface RootProps {
 }
 
 const Root = ({}: RootProps) => {
     return (
         <CssBaseline>
-            <FlexBox sx={{width: "100%", height: "100%", backgroundColor: "rgb(255,247,240)"}}
-                     horizontalAlignment={"center"} verticalAlignment={"center"}>
-                <Outlet/>
-            </FlexBox>
+            <Outlet/>
         </CssBaseline>
     );
 };
