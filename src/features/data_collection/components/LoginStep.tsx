f5e7baa82a1f671315716d2a965248e0ca9c4e38
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-04-29 18:33:01 -0400

    Add instructions to LoginStep

--- src/features/data_collection/components/LoginStep.tsx
+++ src/features/data_collection/components/LoginStep.tsx
@@ -1,6 +1,7 @@
-import {Button} from "@mui/material";
+import {Button, Grid, LinearProgress, Stack, Typography} from "@mui/material";
 import Step from "./stepper/Step.tsx";
 import {useSetStepState} from "./stepper/RouteStepper.tsx";
+import Box from "@mui/material/Box";
 
 interface LoginStepProps {
 
@@ -10,10 +11,30 @@ const LoginStep = ({}: LoginStepProps) => {
     const setStepCompleted = useSetStepState();
 
     return (
-        <Step title={"Login"}>
-            <Button onClick={() => setStepCompleted?.(true)}>Login</Button>
-            <Button onClick={() => setStepCompleted?.(false)}>Logout</Button>
-        </Step>
+        <Grid item xs={8} sm={6} md={5} lg={4} xl={4}>
+            <Step title={"Login"}>
+                <Stack spacing={2}>
+                    <Typography variant={"body1"}>
+                        The tool window should have opened. Please login as you normally would.
+                        Once you have logged in, the tool will display a message to let you know that you can now come
+                        back
+                        to this window.
+                    </Typography>
+                    <Box>
+                        <Typography variant={"body1"} fontWeight={600}>
+                            Please, do not close the tool window after logging in!
+                        </Typography>
+                        <Typography variant={"body1"}>
+                            You can minimize it instead.
+                        </Typography>
+                    </Box>
+                </Stack>
+                <Box my={4} display={"flex"} flexDirection={"column"} alignItems={"center"}>
+                    <Typography variant={"body2"} my={1}>Waiting for login...</Typography>
+                    <LinearProgress sx={{width: "100%"}}/>
+                </Box>
+            </Step>
+        </Grid>
     );
 };
 
