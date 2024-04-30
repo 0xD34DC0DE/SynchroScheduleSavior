cfc3b9fd602f4019f09ebd08fa426cc4fc2910b5
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-04-30 14:49:24 -0400

    use better state management to fix cancellation before running state causing the pipeline to still run.

--- src/lib/webview_scraper/src/pipeline/task_pipeline.ts
+++ src/lib/webview_scraper/src/pipeline/task_pipeline.ts
@@ -19,10 +19,11 @@ type OnPipelineStateChangeCallback = (state: PipelineState) => void;
 
 class TaskPipeline {
     private readonly _target: WebviewWindow;
+    private _pipeline_state: PipelineState = PipelineState.IDLE;
     private readonly _on_state_change?: OnPipelineStateChangeCallback;
     private readonly _steps: PipelineStep[] = [];
+    private _currently_executing_step: PipelineStep | null = null;
     private _window_close_unlisten: UnlistenFn | null = null;
-    private _cancel: (() => void) | null = null;
 
     constructor(target: WebviewWindow, on_state_change?: OnPipelineStateChangeCallback) {
         this._target = target;
@@ -31,46 +32,58 @@ class TaskPipeline {
 
     public execute(on_complete?: OnCompleteCallback): CancelFn {
         this._target.once("tauri://destroyed", () => {
-            this._cancel?.();
-        }).then(unlisten => {
+            this._cancel_execution();
+        }).then(async unlisten => {
             this._window_close_unlisten = unlisten;
-            this._on_state_change?.(PipelineState.RUNNING);
-            this._execute_steps().then(() => {
-                on_complete?.();
-                this._on_state_change?.(PipelineState.DONE);
-            }).catch(e => {
-                if ((e as Error | undefined)?.name !== "CancelledError") throw e;
-                this._on_state_change?.(PipelineState.CANCELLED);
-            });
+            await this._execute_steps();
+            on_complete?.();
+        }).catch(e => {
+            if ((e as Error | undefined)?.name !== "CancelledError") throw e;
         });
 
-        return () => this._cancel?.();
+        return () => this._cancel_execution();
     }
 
     private async _execute_steps(): Promise<void> {
-        let cancelled = false;
+        if (this._pipeline_state !== PipelineState.IDLE) return;
+        this._set_pipeline_state(PipelineState.RUNNING);
+
         for (let step of this._steps) {
-            this._cancel = () => {
-                try {
-                    step.cancel();
-                } catch (e) {
-                    if ((e as Error | undefined)?.name !== "CancelledError") throw e;
-                    cancelled = true;
-                }
-                this._window_close_unlisten?.();
-                this._window_close_unlisten = null;
-                this._cancel = null;
-                this._on_state_change?.(PipelineState.CANCELLED);
-            }
-
-            if (cancelled) break;
-
-            await step.execute(this._target);
+            // TypeScript marks this as an unintentional comparison since the state is set to RUNNING just before
+            // (but the value can change since it's running in a different asynchronous context)
+            // @ts-ignore
+            if (this._pipeline_state !== PipelineState.RUNNING) return;
+            await this._execute_step(step);
         }
 
         this._window_close_unlisten?.();
         this._window_close_unlisten = null;
-        this._cancel = null;
+
+        this._set_pipeline_state(PipelineState.DONE);
+    }
+
+    private _set_pipeline_state(state: PipelineState): void {
+        this._pipeline_state = state;
+        this._on_state_change?.(state);
+    }
+
+    private async _execute_step(step: PipelineStep): Promise<void> {
+        this._currently_executing_step = step;
+        await step.execute(this._target).then(() => {
+            this._currently_executing_step = null;
+        });
+    }
+
+    private _cancel_execution(): void {
+        if (this._pipeline_state === PipelineState.CANCELLED ||
+            this._pipeline_state === PipelineState.DONE) return;
+
+        this._currently_executing_step?.cancel();
+
+        this._window_close_unlisten?.();
+        this._window_close_unlisten = null;
+
+        this._set_pipeline_state(PipelineState.CANCELLED);
     }
 
     public navigate_to(url: string, url_pattern?: string): TaskPipeline {
