fa2fb4fe41eb250d412e6a836596453c5cdd677d
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-04-30 12:53:03 -0400

    fix type error

--- src/components/navigation/Link.tsx
+++ src/components/navigation/Link.tsx
@@ -2,16 +2,21 @@ import {default as MuiLink, LinkProps as MuiLinkProps} from "@mui/material/Link"
 import {LinkProps as RouterLinkProps, Link as RouterLink} from 'react-router-dom';
 import {forwardRef} from "react";
 
-interface LinkProps extends RouterLinkProps, Omit<MuiLinkProps, 'to' | 'component'> {
+interface LinkProps extends Omit<RouterLinkProps, 'color'>, Omit<MuiLinkProps, 'to' | 'component'> {
 
 }
 
-const ForwardedRouterLink = forwardRef<HTMLAnchorElement, LinkProps>((props: LinkProps, ref) => {
-    return <RouterLink {...props} ref={ref}/>;
-});
+type ForwardedRouteLinkProps = Omit<RouterLinkProps, 'color'>;
+
+const ForwardedRouterLink = forwardRef<HTMLAnchorElement, ForwardedRouteLinkProps>(
+    (props: ForwardedRouteLinkProps, ref) => {
+        return <RouterLink {...props} ref={ref}/>;
+    }
+);
 
 const Link = (props: LinkProps) => {
-    return <MuiLink component={ForwardedRouterLink} {...props} />;
+    const {color, ...rest} = props;
+    return <MuiLink color={color} component={ForwardedRouterLink} {...rest} />;
 };
 
 export default Link;
