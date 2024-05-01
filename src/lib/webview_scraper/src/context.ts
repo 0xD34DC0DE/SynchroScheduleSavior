ee46aa4971cf0ebdda9c26262ba3317daf716ae4
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-05-01 13:18:20 -0400

    add mutation observer to default context

--- src/lib/webview_scraper/src/context.ts
+++ src/lib/webview_scraper/src/context.ts
@@ -1,4 +1,6 @@
 import {WebviewWindow} from "@tauri-apps/api/window";
+import {ObservedMutation} from "./pipeline/types.ts";
+//import {ObservedMutation} from "./pipeline/steps/task_with_condition.ts";
 
 /**
  * The label used to identify the window that initiated the scraper
@@ -24,6 +26,48 @@ class Context {
         }
         return initiator;
     }
+
+    public observe_condition(config: ConditionConfig): void {
+        const element = document.querySelector(config.selector);
+        if (!element) {
+            throw new Error(`Element not found: ${config.selector}`);
+        }
+
+        const initiator = this.initiator;
+
+        const observer = new MutationObserver(async (mutations) => {
+            const observed_mutations: ObservedMutation[] = mutations.map(mutation => ({
+                target: mutation.target.toString(),
+                attributeName: mutation.attributeName,
+                oldValue: mutation.oldValue,
+                currentValue: (mutation.target as Element).getAttribute(mutation.attributeName ?? ""),
+            }));
+
+            await initiator.emit(config.condition_event_id, observed_mutations);
+        });
+
+        let listening = true;
+
+        initiator.once(config.condition_met_event_id, () => {
+            observer.disconnect();
+            listening = false;
+        }).then(unlisten =>
+            setTimeout(() => {
+                unlisten();
+                if (listening) throw new Error("Observer time out");
+            }, 10000)
+        );
+
+        observer.observe(element, config.mutation_observer_config);
+    }
 }
 
+export type {ConditionConfig};
 export default Context;
+
+type ConditionConfig = {
+    condition_event_id: string;
+    condition_met_event_id: string;
+    selector: string;
+    mutation_observer_config: MutationObserverInit;
+}
