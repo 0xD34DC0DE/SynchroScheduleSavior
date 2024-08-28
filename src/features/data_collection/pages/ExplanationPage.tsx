5517b8ec8f12f11e261f8f384a3bad40a231f211
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-08-28 13:25:32 -0400

    start implementation of explanation animation

--- src/features/data_collection/pages/ExplanationPage.tsx
+++ src/features/data_collection/pages/ExplanationPage.tsx
@@ -1,13 +1,20 @@
 import {ButtonLink} from "../../../components/navigation";
-import {Grid, Typography} from "@mui/material";
+import {Button, Grid, Typography} from "@mui/material";
 import TooltipTypography from "../components/TooltipTypography.tsx";
 import ExplanationAnimation from "../components/ExplanationAnimation.tsx";
+import {useState} from "react";
+import {GridProps} from "@mui/material/Grid/Grid";
 
 interface ExplanationPageProps {
     startPath: string;
 }
 
 const ExplanationPage = ({startPath}: ExplanationPageProps) => {
+    const [step, setStep] = useState(0);
+    const steps = [Step1, Step2, Step3];
+
+    const isLastStep = step === steps.length - 1;
+
     return (
         <Grid container item xs={12} sm={10} spacing={1} alignItems="center">
             <Grid container item xs={5} spacing={2}>
@@ -17,86 +24,143 @@ const ExplanationPage = ({startPath}: ExplanationPageProps) => {
                     </Typography>
                 </Grid>
 
-                <Grid item xs={12}>
-                    <Typography variant={"h4"} fontWeight={600}>
-                        Step 1: Login
-                    </Typography>
-                    <Typography variant={"body1"}>
-                        Once the tool is started, a new window will open at the login page.
-                        Login as you normally would.
-                        Don't worry,&nbsp;
-                        <TooltipTypography
-                            title={
-                                "When you enter your credentials," +
-                                " they are sent directly to the website you are logging into." +
-                                " To prevent your credentials from being compromised, the tool does not store," +
-                                " log or interact with them. This has the side effect that you will need to log in" +
-                                " every time you start the data collection tool."
-                            }
-                            maxWidth={500}
-                            display={"inline"}
-                            component={"span"}
-                            underline
+                {steps.map((Step, index) => (
+                    <Step key={index} visibility={index > step ? "hidden" : "visible"}/>
+                ))}
+
+                <Grid
+                    item
+                    xs={12}
+                    mt={2}
+                    justifyContent={"space-around"}
+                    display={"flex"}
+                    gap={2}
+                    alignItems={"center"}
+                >
+                    <Button
+                        onClick={() => setStep(step - 1)}
+                        variant="outlined"
+                        sx={{px: 3, fontSize: "110%", width: "30%"}}
+                        disabled={step === 0}
+                    >
+                        Back
+                    </Button>
+                    {!isLastStep &&
+                        <Button
+                            onClick={() => setStep(step + 1)}
+                            variant="contained"
+                            sx={{px: 3, fontSize: "110%", width: "30%"}}
+                            disabled={isLastStep}
                         >
-                            this tool does not store your login information.
-                        </TooltipTypography>
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <Typography variant={"h4"} fontWeight={600}>
-                        Step 2: Login confirmation
-                    </Typography>
-                    <Typography variant={"body1"}>
-                        One logged in, the tool will let you know that you have successfully logged in.
-                        At this point,&nbsp;
-                        <TooltipTypography
-                            title={
-                                "The tool window needs to stay open to collect data." +
-                                " See the next step for more information."
-                            }
-                            maxWidth={500}
-                            display={"inline"}
-                            component={"span"}
-                            underline
+                            Next
+                        </Button>
+                    }
+
+                    {isLastStep &&
+                        <ButtonLink
+                            to={startPath}
+                            variant="contained"
+                            color="success"
+                            sx={{px: 3, fontSize: "110%", width: "30%"}}
                         >
-                            do not close the tool window!
-                        </TooltipTypography>
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <Typography variant={"h4"} fontWeight={600}>
-                        Step 3: Data collection
-                    </Typography>
-                    <Typography variant={"body1"}>
-                        Once you are back in the application window, the tool will start collecting data.
-                        This process is&nbsp;
-                        <TooltipTypography
-                            title={
-                                "To reduce the time it takes to collect data, the tool will only collect relevant" +
-                                " data. This means that the application will ask you to perform choices so that it" +
-                                " can instruct the tool on what data to collect. The interactions are done on the" +
-                                " application window, not the tool window."
-                            }
-                            maxWidth={500}
-                            display={"inline"}
-                            component={"span"}
-                            underline
-                        >
-                            interactive
-                        </TooltipTypography>
-                        &nbsp;and will guide you through the data collection process.
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} mt={2} justifyContent={"center"} display={"flex"}>
-                        <ButtonLink to={startPath} variant="contained" sx={{px: 3, fontSize: "110%"}}>Start</ButtonLink>
+                            Begin
+                        </ButtonLink>
+                    }
+
+
                 </Grid>
             </Grid>
 
             <Grid item xs={7}>
-                <ExplanationAnimation/>
+                <ExplanationAnimation step={0}/>
             </Grid>
         </Grid>
     );
 };
 
 export default ExplanationPage;
+
+function Step1(props: GridProps) {
+    return (
+        <Grid item {...props}>
+            <Typography variant={"h4"} fontWeight={600}>
+                Step 1: Login
+            </Typography>
+            <Typography variant={"body1"}>
+                Once the tool is started, a new window will open at the login page.
+                Login as you normally would.
+                Don't worry,&nbsp;
+                <TooltipTypography
+                    title={
+                        "When you enter your credentials," +
+                        " they are sent directly to the website you are logging into." +
+                        " To prevent your credentials from being compromised, the tool does not store," +
+                        " log or interact with them. This has the side effect that you will need to log in" +
+                        " every time you start the data collection tool."
+                    }
+                    maxWidth={500}
+                    display={"inline"}
+                    component={"span"}
+                    underline
+                >
+                    this tool does not store your login information.
+                </TooltipTypography>
+            </Typography>
+        </Grid>
+    );
+}
+
+function Step2(props: GridProps) {
+    return (
+        <Grid item {...props}>
+            <Typography variant={"h4"} fontWeight={600}>
+                Step 2: Login confirmation
+            </Typography>
+            <Typography variant={"body1"}>
+                One logged in, the tool will let you know that you have successfully logged in.
+                At this point,&nbsp;
+                <TooltipTypography
+                    title={
+                        "The tool window needs to stay open to collect data." +
+                        " See the next step for more information."
+                    }
+                    maxWidth={500}
+                    display={"inline"}
+                    component={"span"}
+                    underline
+                >
+                    do not close the tool window!
+                </TooltipTypography>
+            </Typography>
+        </Grid>
+    );
+}
+
+function Step3(props: GridProps) {
+    return (
+        <Grid item {...props}>
+            <Typography variant={"h4"} fontWeight={600}>
+                Step 3: Data collection
+            </Typography>
+            <Typography variant={"body1"}>
+                Once you are back in the application window, the tool will start collecting data.
+                This process is&nbsp;
+                <TooltipTypography
+                    title={
+                        "To reduce the time it takes to collect data, the tool will only collect relevant" +
+                        " data. This means that the application will ask you to perform choices so that it" +
+                        " can instruct the tool on what data to collect. The interactions are done on the" +
+                        " application window, not the tool window."
+                    }
+                    maxWidth={500}
+                    display={"inline"}
+                    component={"span"}
+                    underline
+                >
+                    interactive
+                </TooltipTypography>
+                &nbsp;and will guide you through the data collection process.
+            </Typography>
+        </Grid>
+    );
+}
