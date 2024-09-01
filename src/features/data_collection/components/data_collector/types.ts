e7dd2fffa7686ac8d3c170dc1910d404d0165453
Émerick Poulin <31230799+0xD34DC0DE@users.noreply.github.com>
2024-09-01 13:11:39 -0400

    add more fields to gathered data model

--- src/features/data_collection/components/data_collector/types.ts
+++ src/features/data_collection/components/data_collector/types.ts
@@ -6,12 +6,16 @@ export type CourseBlockData = {
     remaining_credits: number;
 }
 
-export type BaseCourseData = {
+export type BaseCourseData<SectionDataType = SectionData> = {
     id: string;
     name: string;
     credits: number;
-    block_id?: CourseBlockData["id"];
     status: "not taken";
+    block_id?: CourseBlockData["id"];
+    prerequisites?: BaseCourseData["id"][];
+    corequisites?: BaseCourseData["id"][];
+    description?: string;
+    sections?: SectionDataType[];
 }
 
 export type TakenCourseData = Omit<BaseCourseData, "status"> & {
@@ -20,4 +24,56 @@ export type TakenCourseData = Omit<BaseCourseData, "status"> & {
     grade: string;
 }
 
-export type CourseData = BaseCourseData | TakenCourseData;
+export type CourseData<SectionDataType = SectionData> = BaseCourseData<SectionDataType> | TakenCourseData;
+
+type ScraperCourseLinkData = {
+    course_link_id: string;
+}
+
+export type ScraperCourseData = CourseData<ScraperSectionTypeData> & ScraperCourseLinkData;
+
+export type SectionData<SectionTypeDataType = SectionTypeData> = {
+    id: string;
+    associated_section_group: number;
+    status: "open" | "closed";
+    schedule: SectionScheduleData[];
+    type: "TH" | "TP" | "LAB";
+} & SectionTypeDataType;
+
+export type SectionScheduleData = {
+    start_time: string;
+    end_time: string;
+    day: string;
+    start_date: string;
+    end_date: string;
+    location: string;
+    teacher: string;
+}
+
+export type TheoreticalSectionData = {
+    type: "TH";
+    exams: ExamScheduleData[];
+}
+export type ScraperTheoreticalSectionData = TheoreticalSectionData & {
+    course_detail_link_id: string;
+}
+
+export type PracticalSectionData = {
+    type: "TP";
+}
+
+export type LaboratorySectionData = {
+    type: "LAB";
+}
+
+export type SectionTypeData = TheoreticalSectionData | PracticalSectionData | LaboratorySectionData;
+export type ScraperSectionTypeData = ScraperTheoreticalSectionData | PracticalSectionData | LaboratorySectionData;
+
+export type ExamScheduleData = {
+    start_time: string;
+    end_time: string;
+    day: string;
+    date: string;
+    location: string;
+    type: "final" | "intra";
+}
